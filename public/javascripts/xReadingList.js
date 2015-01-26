10238b23e7838290e7730ecf345375ab0ed87d71
Kelly <kellytweten@gmail.com>
2015-01-26 14:16:10 -0800

    Fixing the top of an open selection when scrolling past so it can be closed easily.

--- public/javascripts/xReadingList.js
+++ public/javascripts/xReadingList.js
@@ -51,3 +51,23 @@ xReadingListApp.controller('QueryController', ['$http', 'NameList', 'Collection'
         });
     };
 }]);
+
+$(function () {
+    $(window).scroll(function () {
+        var windowTop = $(window).scrollTop();
+
+        $('.fixed-title').each(function() {
+            if (windowTop > $(this).parent().offset().top) {
+                $('.fixed-title').removeClass('fixed');
+                $(this).addClass('fixed');
+            } else {
+                $(this).removeClass('fixed');
+            }
+        });
+    });
+
+    $(".selecting-title").click( function() {
+        $(this).toggleClass('fixed-title');
+        $(this).removeClass('fixed');
+    });
+});
