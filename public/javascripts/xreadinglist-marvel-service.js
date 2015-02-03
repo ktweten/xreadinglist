3fb07949733ef000da656ef53db4682c241af015
Kelly <kellytweten@gmail.com>
2015-02-02 18:45:39 -0800

    Testing multiple issue query approach again.

--- public/javascripts/xreadinglist-marvel-service.js
+++ public/javascripts/xreadinglist-marvel-service.js
@@ -22,7 +22,65 @@ function mapUrls(urls) {
 }
 
 angular.module('xReadingList').service('MarvelService', ['$http', function($http) {
-     function makeIssueCallback(issue, col) {
+    var data = {};
+
+    function getVolumeData(series, startYear) {
+        var seriesEntry;
+
+        if (!data[series]) {
+            data[series] = {};
+        }
+        seriesEntry = data[series];
+
+        if (!seriesEntry[startYear]) {
+            seriesEntry[startYear] = {}
+        }
+        return seriesEntry[startYear];
+    }
+
+    //function getIssueData(series, startYear, number) {
+    //    var yearEntry = getVolumeData(series, startYear);
+    //
+    //    if (!yearEntry[number]) {
+    //        yearEntry[number] = {
+    //            coverRoot: "",
+    //            extension: "",
+    //            urls: mapUrls([])
+    //        };
+    //    }
+    //
+    //    return yearEntry[number];
+    //}
+
+    function makeSetDataCallback(volume) {
+        var foundVolume = volume;
+
+        return function(res, status, headers, config) {
+            var i,
+                offset = 0,
+                foundIssue;
+
+            if (res.data && res.data.results) {
+                for (i = 0; i < res.data.results.length; i += 1) {
+                    if (!foundVolume[res.data.results[i].issueNumber]) {
+                        foundVolume[res.data.results[i].issueNumber] = {};
+                        foundIssue = foundVolume[res.data.results[i].issueNumber];
+
+                        foundIssue.coverRoot = res.data.results[i].thumbnail.path;
+                        foundIssue.extension = res.data.results[i].thumbnail.extension;
+                        foundIssue.urls = mapUrls(res.data.results[i].urls);
+                    }
+                }
+
+                offset = res.data.offset + res.data.count;
+                if (res.data.total > offset) {
+
+                }
+            }
+        }
+    }
+
+    function makeIssueCallback(issue) {
         var foundIssue = issue;
 
         return function(res, status, headers, config) {
@@ -37,14 +95,19 @@ angular.module('xReadingList').service('MarvelService', ['$http', function($http
     function getMarvelData(issue) {
         var link = 'http://gateway.marvel.com:80/v1/public/comics?title=' + issue.series +
             '&startYear=' + issue.volume +
-            '&issueNumber=' + issue.number +
+            //'&issueNumber=' + issue.number +
             '&noVariants=true' +
-            '&apikey=2c7b5e832ec9ddc7c4dc4e432f24fbb4';
+            '&apikey=2c7b5e832ec9ddc7c4dc4e432f24fbb4',
+            volumeData = getVolumeData(issue.series, issue.volume);
 
-        $http.get(link, { cache: true }).success(makeIssueCallback(issue)).
-        error(function(data, status, headers, config) {
-            //?
-        });
+        if (volumeData[issue.number]) {
+            issue.coverRoot = volumeData[issue.number].coverRoot;
+            issue.extension = volumeData[issue.number].extension;
+            issue.urls = volumeData[issue.number].urls;
+        } else {
+            $http.get(link, { cache: true }).success(makeSetDataCallback(volumeData));
+        }
+        //$http.get(link, { cache: true }).success(makeIssueCallback(issue));
     }
 
     return {
